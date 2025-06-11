6222b7b5e6b9f5a53ece46ae5fec314be49c04f0
Maayan Zahavi <maayan.zahavi@hotmail.co.il>
2025-06-11 11:22:55 +0300

    build added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const http = require('http');
 const { Server } = require('socket.io');
 const jwt = require('jsonwebtoken');
+const path = require('path');
+
 require('dotenv').config();
 
 const app = express();
@@ -55,6 +57,11 @@ module.exports.io = io;
 app.use(cors());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 
 // === ROUTES ===
 app.use("/api/loadBalance", require("./routes/loadBalance"));
